bb646b925ea477a32c1ed8b759f94743cf0787c9
Balaji Eswaramoorthi <balajisubasheckp@outlook.com>
2023-09-01 13:26:34 +0800

    figlet script

---
+++ figlet.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if command -v figlet &> /dev/null
+then
+	figlet balaji
+else
+	echo "   _     _     _     _     _     _  "
+	echo "  / \   / \   / \   / \   / \   / \ "
+	echo " ( b ) ( a ) ( l ) ( a ) ( j ) ( i )"
+	echo "  \_/   \_/   \_/   \_/   \_/   \_/ "
+fi
