4ba946b98afc78f7b4b5fead4877076bf70956af
beswaramurthy <beswaramurthy@maxlinear.com>
2023-09-27 18:10:41 +0800

    scripts: modify debug utils

--- debug.sh
+++ debug.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
-DEBUG=( "" "debug" "info" "warning" "error" )
+source /home/$(whoami)/scripts/enum.sh
 
-debug=0
+enum __log_level { LOG_DEBUG, LOG_INFO, LOG_WARNING, LOG_ERROR }
 
-log_level=1
+log_string=( "DEBUG" "INFO" "WARNING" "ERROR" )
+
+log_level=$LOG_ERROR
 
 function execute () {
-	if [ $debug -gt 0 ]; then
-		echo ${DEBUG[$debug]} executing "$@"
-	fi
+	echo EXECUTE "$@"
 	$@
+}
+
+function print_error () {
+	if [ $log_level -ge $LOG_ERROR ]; then
+		echo ${log_string[$LOG_ERROR]} "$@"
+	fi
+}
+
+function print_warning () {
+	if [ $log_level -ge $LOG_WARNING ]; then
+		echo ${log_string[$LOG_WARNING]} "$@"
+	fi
+}
+
+function print_info () {
+	if [ $log_level -ge $LOG_INFO ]; then
+		echo ${log_string[$LOG_INFO]} "$@"
+	fi
+}
+
+function print_debug () {
+	if [ $log_level -ge $LOG_DEBUG ]; then
+		echo ${log_string[$LOG_DEBUG]} "$@"
+	fi
 }
---
+++ enum.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This funcion allow to declare enum "types"
+enum ()
+{
+    # skip index ???
+    shift
+    AA=${@##*\{} # get string strip after { 
+    AA=${AA%\}*} # get string strip before }
+    AA=${AA//,/} # delete commaa  
+    ((DEBUG)) && echo $AA
+    local I=0
+    for A in $AA ; do
+        eval "$A=$I"
+        ((I++))
+    done
+}
